fa8efec98d0c7fda2c3a7709f6ff72142dffb38e
ruijin-lixian <53926220+ruijin-lixian@users.noreply.github.com>
2023-11-09 20:21:16 +0800

    Update with-status.vue

--- docs/examples/table/with-status.vue
+++ docs/examples/table/with-status.vue
@@ -39,9 +39,7 @@ const tableData: User[] = [
     address: 'No. 189, Grove St, Los Angeles',
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+
   },
   {
     date: '2016-05-04',
